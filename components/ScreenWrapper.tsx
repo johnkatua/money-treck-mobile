bf1cac2279b388cc3598626fea494db51a598018
johnkatua <johnkatua99@gmail.com>
2025-03-03 13:19:17 +0300

    feat: enhance ScreenWrapper with dynamic styling and background color

--- components/ScreenWrapper.tsx
+++ components/ScreenWrapper.tsx
@@ -2,12 +2,22 @@ import { Platform, StyleSheet, Text, View } from 'react-native';
 import React, { FC } from 'react';
 import { ScreenContainerProps } from 'react-native-screens';
 import { SCREEN_HEIGHT } from '../utils/styling';
+import { colors } from '../constants/theme';
 
 const ScreenWrapper: FC<ScreenContainerProps> = ({ style, children }) => {
   const paddingTop = Platform.OS === 'ios' ? SCREEN_HEIGHT * 0.06 : 50;
   return (
-    <View style={style}>
-      <Text>ScreenWrapper</Text>
+    <View
+      style={[
+        {
+          paddingTop,
+          flex: 1,
+          backgroundColor: colors.neutral900,
+        },
+        style,
+      ]}
+    >
+      {children}
     </View>
   );
 };
