958228348a2eda07f227aa6d25eb46011a38595a
johnkatua <johnkatua99@gmail.com>
2025-03-03 11:03:19 +0300

    feat: update Prettier configuration and enhance styling utility with scaling functions

--- utils/styling.ts
+++ utils/styling.ts
@@ -1,3 +1,19 @@
-import { Dimensions, PixelRatio } from 'react-native'
+import { Dimensions, PixelRatio } from 'react-native';
 
-const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window')
+const { width: SCREEN_WIDTH, height: SCREEN_HEIGHT } = Dimensions.get('window');
+
+const [shortDimension, longDimension] =
+  SCREEN_WIDTH < SCREEN_HEIGHT ? [SCREEN_WIDTH, SCREEN_HEIGHT] : [SCREEN_HEIGHT, SCREEN_WIDTH];
+
+const guidelineBaseWidth = 375;
+const guidelineBaseHeight = 812;
+
+export const horizontalScale = (size: number) =>
+  Math.round(
+    PixelRatio.roundToNearestPixel(((shortDimension / guidelineBaseWidth) * size) as number)
+  );
+
+export const verticalScal = (size: number) =>
+  Math.round(
+    PixelRatio.roundToNearestPixel(((longDimension / guidelineBaseHeight) * size) as number)
+  );
