ced03d8abded42ed305d16263a6209d07e391b87
johnkatua <johnkatua99@gmail.com>
2025-03-03 12:54:58 +0300

    refactor: remove unused Text import from index component

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, View } from 'react-native';
 import { colors } from '../constants/theme';
 
 const index = () => {
