5bdb568c87a62421ca4e7051d187b0d649527ab7
johnkatua <johnkatua99@gmail.com>
2025-03-04 18:12:13 +0300

    feat: add BackButton component with basic structure and props definition

--- types.ts
+++ types.ts
@@ -26,3 +26,8 @@ export interface CustomButtonProps extends TouchableOpacityProps {
   children: ReactNode;
   onPress?: () => void;
 }
+
+export type BackButtonProps = {
+  style?: ViewStyle;
+  iconSize: number;
+};
