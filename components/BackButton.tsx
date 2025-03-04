0cfdc878aab6745307376ff8de32f3589fabf4b2
johnkatua <johnkatua99@gmail.com>
2025-03-04 18:34:00 +0300

    feat: enhance BackButton styling with background color and border radius

--- components/BackButton.tsx
+++ components/BackButton.tsx
@@ -4,7 +4,7 @@ import { BackButtonProps } from '../types';
 import { useRouter } from 'expo-router';
 import { CaretLeft } from 'phosphor-react-native';
 import { verticalScale } from '../utils/styling';
-import { colors } from '../constants/theme';
+import { colors, radius } from '../constants/theme';
 
 const BackButton: FC<BackButtonProps> = ({ style, iconSize = 26 }) => {
   const router = useRouter();
@@ -18,5 +18,11 @@ const BackButton: FC<BackButtonProps> = ({ style, iconSize = 26 }) => {
 export default BackButton;
 
 const styles = StyleSheet.create({
-  button: {},
+  button: {
+    backgroundColor: colors.neutral600,
+    alignSelf: 'flex-start',
+    borderRadius: radius._12,
+    borderCurve: 'continuous',
+    padding: 5,
+  },
 });
