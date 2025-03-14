772115704a5a26c74bb17fb2ec72406501b17f20
johnkatua <johnkatua99@gmail.com>
2025-03-14 19:34:47 +0300

    fix: update API base query to include headers preparation logic

--- api/rtkApi.ts
+++ api/rtkApi.ts
@@ -2,14 +2,17 @@ import { appConfig } from '../config/app-config';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: appConfig.apiBaseUrl }),
-  prepareHeaders: (headers, { getState }) => {
-    const token = getState().auth.token;
-    if (token) {
-      headers.set('authorization', `Bearer ${token}`);
-    }
-    return headers;
-  },
+  baseQuery: fetchBaseQuery({
+    baseUrl: appConfig.apiBaseUrl,
+    prepareHeaders: (headers, { getState }) => {
+      const token = '';
+      // const token = getState().auth.token;
+      if (token) {
+        headers.set('authorization', `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
   endpoints: (builder) => ({}),
   reducerPath: 'api',
   tagTypes: [],
