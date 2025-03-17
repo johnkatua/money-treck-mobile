174ba91a081e5e65883a3b0bcba0fa7347b6ae6c
johnkatua <johnkatua99@gmail.com>
2025-03-17 16:35:15 +0300

    feat: add logout button to Home component with dispatch action

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Button from '../../components/Button';
+import Typo from '../../components/Typo';
+import { useAppDispatch } from '../../store/root';
+import { logout } from '../../features/auth/authSlice';
 
 const Home = () => {
+  const dispatch = useAppDispatch();
   return (
     <View>
       <Text>Home</Text>
+      <Button onPress={() => dispatch(logout())}>
+        <Typo>Logout</Typo>
+      </Button>
     </View>
   );
 };
