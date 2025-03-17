38aba5cdf28dd2d1322cfb7b60388e3016913d73
johnkatua <johnkatua99@gmail.com>
2025-03-17 20:23:14 +0300

    feat: update MTSnackbar props

--- components/MTSnackbar.tsx
+++ components/MTSnackbar.tsx
@@ -1,10 +1,16 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
+import React, { FC } from 'react';
+import { StyleSheet, Text } from 'react-native';
 import { Snackbar } from 'react-native-paper';
+import { MTSnackbarProps } from '../types';
 
-const MTSnackbar = ({ message, visible, onDismissSnackBar }) => {
+const MTSnackbar: FC<MTSnackbarProps> = ({
+  duration = 3000,
+  message,
+  visible,
+  onDismissSnackBar,
+}) => {
   return (
-    <Snackbar visible={visible} onDismiss={onDismissSnackBar}>
+    <Snackbar duration={duration} visible={visible} onDismiss={onDismissSnackBar}>
       <Text>{message}</Text>
     </Snackbar>
   );
