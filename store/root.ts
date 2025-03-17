b12012416cf8efe2897287067947fe500f53c847
johnkatua <johnkatua99@gmail.com>
2025-03-17 15:37:21 +0300

    feat: integrate authSlice into root reducer for user authentication

--- store/root.ts
+++ store/root.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { api } from '../api/rtkApi';
+import authSlice from '../features/auth/authSlice';
 
 export default combineReducers({
+  auth: authSlice,
   [api.reducerPath]: api.reducer,
 });
