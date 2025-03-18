68edb997d470bddafe8ca084789499038f8cb350
johnkatua <johnkatua99@gmail.com>
2025-03-18 21:10:51 +0300

    feat: update Header component layout with improved styles and icon handling

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,12 +5,35 @@ import { HeaderProps } from '../types';
 
 const Header: FC<HeaderProps> = ({ title = '', leftIcon, style }) => {
   return (
-    <View>
-      <Typo>Header</Typo>
+    <View style={[styles.container, style]}>
+      {leftIcon && <View style={styles.leftIcon}>{leftIcon}</View>}
+      {title && (
+        <Typo
+          size={22}
+          fontWeight={'600'}
+          style={{ width: leftIcon ? '80%' : '100%', textAlign: 'center' }}
+        >
+          {title}
+        </Typo>
+      )}
     </View>
   );
 };
 
 export default Header;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 20,
+  },
+  title: {
+    textAlign: 'center',
+  },
+  leftIcon: {
+    width: 20,
+    height: 20,
+  },
+});
