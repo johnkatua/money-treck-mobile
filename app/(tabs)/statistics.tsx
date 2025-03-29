3e4bd7ad3d0b431a088b26fa83000b6393a615fb
johnkatua <johnkatua99@gmail.com>
2025-03-29 09:52:43 +0300

    feat: enhance Statistics screen layout with Header and ScrollView integration

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,15 +1,45 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import React from 'react';
 import ScreenWrapper from '../../components/ScreenWrapper';
+import { colors, spacingX, spacingY } from '../../constants/theme';
+import Header from '../../components/Header';
+import Typo from '../../components/Typo';
 
 const Statistics = () => {
   return (
     <ScreenWrapper>
-      <Text>Statistics</Text>
+      <View style={styles.container}>
+        <View
+          style={{
+            borderBottomColor: colors.neutral700,
+            borderWidth: 1,
+          }}
+        >
+          <Header
+            title="Statistics"
+            style={{
+              marginBottom: spacingY._10,
+            }}
+          />
+        </View>
+        <ScrollView>
+          <View style={{ height: 900 }}>
+            <Typo>Statistics</Typo>
+          </View>
+        </ScrollView>
+      </View>
     </ScreenWrapper>
   );
 };
 
 export default Statistics;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  contentContainer: {
+    paddingHorizontal: spacingX._10,
+    height: 'auto',
+  },
+});
