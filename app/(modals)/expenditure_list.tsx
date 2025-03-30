1000f4ab3d07fec9e385a521e98686bdf97d5502
johnkatua <johnkatua99@gmail.com>
2025-03-30 19:49:54 +0300

    feat: add ExpenditureList component for displaying expenditures

---
+++ app/(modals)/expenditure_list.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ExpenditureList = () => {
+  return (
+    <View>
+      <Text>ExpenditureList</Text>
+    </View>
+  );
+};
+
+export default ExpenditureList;
+
+const styles = StyleSheet.create({});
