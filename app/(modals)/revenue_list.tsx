590ed1e17f617462aa169f1ae7649d1683210c70
johnkatua <johnkatua99@gmail.com>
2025-03-30 19:48:24 +0300

    feat: add a revenue list component for displaying revenues

---
+++ app/(modals)/revenue_list.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const RevenueList = () => {
+  return (
+    <View>
+      <Text>RevenueList</Text>
+    </View>
+  );
+};
+
+export default RevenueList;
+
+const styles = StyleSheet.create({});
