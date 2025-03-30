bcb8ac993ad13ba91ab7b0f18182c7e14b40a731
johnkatua <johnkatua99@gmail.com>
2025-03-30 20:38:47 +0300

    feat: implement RevenueList component with modal wrapper and header

--- app/(modals)/revenue_list.tsx
+++ app/(modals)/revenue_list.tsx
@@ -1,14 +1,29 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import ModalWrapper from '../../components/ModalWrapper';
+import { modalStyles } from '../../utils/styling';
+import Header from '../../components/Header';
+import BackButton from '../../components/BackButton';
+import { spacingY } from '../../constants/theme';
 
 const RevenueList = () => {
   return (
-    <View>
-      <Text>RevenueList</Text>
-    </View>
+    <ModalWrapper>
+      <View style={modalStyles.container}>
+        <Header
+          title="Revenues"
+          leftIcon={<BackButton />}
+          style={{
+            marginBottom: spacingY._10,
+          }}
+        />
+      </View>
+    </ModalWrapper>
   );
 };
 
 export default RevenueList;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {},
+});
